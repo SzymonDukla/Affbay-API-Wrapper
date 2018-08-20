79772897dcb1f9db01c85aac5d538a1e50be4627
Szymon Dukla <7996340+SzymonDukla@users.noreply.github.com>
2018-08-20 12:11:04 +0200

    Another example - second phone number

---
+++ example/second-phone/index.php
@@ -0,0 +1,43 @@
+<?php
+
+use Affbay\AffbayApi\AffbayApi;
+use Affbay\AffbayApi\Contact;
+use Affbay\AffbayApi\AffbayException;
+
+require_once __DIR__ . '/../../AffbayApi.php';
+
+try {
+	
+	$affbay = new AffbayApi('abcabcabc123');
+	
+	/*
+	 * Create new contact
+	 */
+	$contact = new Contact([
+		'first_name' => 'Joe',
+		'last_name' => 'Doe',
+		'phone' => '1234567890',
+		'phone2' => '1234567890',	//	here you pass the second phone parameter
+		'email' => 'joe@example.com',
+		'click_id' => '12345'
+	]);
+	//  Set product SKU (you can do it also by passing `product` with SKU as a value in parameters array when
+	// creating Contact instance
+	$contact->setProduct('a47f1278-9c3c-4551-91b2-b8f7dc526f81');
+	//  Stash newly created contact and prepare it to dispatch
+	$contact->stash();
+	
+	/*
+	 * Send contact to Affbay
+	 */
+	
+	$result = $affbay->push();
+	
+	/*
+	 * Show results
+	 */
+	print_r($result);
+	
+} catch (AffbayException $e) {
+	print $e->getMessage();
+}
